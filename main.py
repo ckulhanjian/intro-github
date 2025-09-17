b7e7081af7b65a19f4dc71b680c4cc8e0664c70b
ckulhanjian <carak143@gmail.com>
2025-09-17 11:28:32 -0400

    git commands

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("github test")
+
+print("Hello WiCSE & SWE!")
+print("Thank you for coming to the GIT workshop!")
+
+
+
+'''
+1. git add () 
+- all the files being changed
+
+2. git commit -m ()
+- save to local repo
+
+3. git push origin main
+- push to virtual repo
+'''
